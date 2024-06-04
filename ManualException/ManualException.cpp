fe1276f69ad352b671fa8a9f9d204f103061447d
Irfansyah001 <syahirfan545@gmail.com>
2024-06-04 15:32:02 +0700

    add catch (int a) dan catch (...)

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -9,3 +9,11 @@ int main()
         cout << "Pernyataan tidak akan dieksekusi" << endl;
 
     }
+    catch (int a) {
+        // blok akan dieksekusi
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /* jika selain int maka block ini akan dieksekusi */
+            cout << "default pengecualian dieksekusi" << endl;
+    }
