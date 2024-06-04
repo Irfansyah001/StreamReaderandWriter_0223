fa62ef9fcd8894be82f6a183e24b49427c77bbfb
Irfansyah001 <syahirfan545@gmail.com>
2024-06-04 15:23:03 +0700

    add int main demngan try pesan array int 3 element, dan memanggil array elemen 5, add catch exception& e dan penangkapan menggunakan objek exception lalu cout baris program yang terakhir lalu return 0

--- ExceptionStandar/ExceptionStandar.cpp
+++ ExceptionStandar/ExceptionStandar.cpp
@@ -4,3 +4,22 @@
 #include <array>
 // untuk objek array yg akan digunakan
 using namespace std;
+
+int main()
+{
+    cout << "awal program" << endl; // penanda 1
+    try {
+        array<int, 3> data = { 3, 5, 7 };
+        //pesan array int 3 element
+        cout << data.at(5) << endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan di eksekusi karena array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+        return 0;
+}
